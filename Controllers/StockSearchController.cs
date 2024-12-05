4c177b9c1aac0979701cbff9267381a55119aecf
34412as@gmail.com <34412as@gmail.com>
2024-12-05 19:47:20 +0200

    fix a bug in stock search

--- Controllers/StockSearchController.cs
+++ Controllers/StockSearchController.cs
@@ -59,12 +59,6 @@ namespace InvestmentPortfolioAPI.Controllers
             {
                 var stockInfo = await _stockService.GetStockInfoAsync(symbol);
 
-                //if (stockInfo == null)
-                //{
-                //    _logger.LogInformation("No stock information found for symbol: {Symbol}", symbol);
-                //    return NotFound($"Stock information not found for symbol: {symbol}");
-                //}
-
                 _logger.LogInformation("Successfully retrieved stock information for symbol: {Symbol}", symbol);
                 return Ok(stockInfo);
             }
